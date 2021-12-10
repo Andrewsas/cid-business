bd24237a5ae27fb31455b0b383aee2fc0ea08596
Andrewsas <s.andrew.santos@gmail.com>
2021-12-09 21:18:16 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
+import os, time
+
+app = Flask(__name__)
+app.secret_key = 'akçsmdakdlçamsd'
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Nova predição')
+
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    return redirect(url_for('novo'))
+
+
+@app.route('/')
+def login():
+    proxima = (request.args.get('proxima') if request.args.get('proxima') != None else '/novo')
+    return render_template('login.html', proxima=proxima)
+
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    
+    if request.form['user'] == os.environ['USER'] and request.form['pass'] == os.environ['PASS']:
+        return redirect(url_for('novo'))
+    else:
+        flash('Não logado, tente denovo!')
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect(url_for('/'))
+
+
+app.run(debug=True)
